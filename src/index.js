00186cb2af274c74dc31ffb85061d395240e05c1
udaykene <udaykene96@gmail.com>
2026-01-22 23:40:07 +0530

    Added the connect method in indexjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-// require('dotenv).config({path:'./env'}) 
-import dotenv from 'dotenv'
+// require('dotenv).config({path:'./env'})
+import dotenv from "dotenv";
 
-import connect from './db/index.js'
-
-
+import connect from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./.env'
-})
-
+  path: "./.env",
+});
 
 connect()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Err", error);
+      throw error;
+    });
 
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!", err);
+  });
 
 /*
 
